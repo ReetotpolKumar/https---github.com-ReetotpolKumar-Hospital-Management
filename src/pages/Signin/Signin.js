4faeb223a3d4efbeeb2b7599bcacc39d8f831810
ReetotpolKumar <reetotpolkumar@gmail.com>
2023-03-15 11:50:23 +0530

    Add Signin page

---
+++ src/pages/Signin/Signin.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import style from "./Signin.module.css"
+
+function Signin() {
+  return (
+    <div>
+        <div className={style.nav}>
+
+        
+       
+
+      
+
+        </div>
+    <div className={style.box}>
+      <input type="text"></input>
+      <button>Submit</button>
+    </div>
+    </div>
+  )
+}
+
+export default Signin
